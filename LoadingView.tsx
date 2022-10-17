8c6e0da1bb34d7d45f9e3d280d4a689e692b481e
Thien Tran <thien.tran@dfo.global>
2022-10-17 21:58:56 +0700

    Loading view

---
+++ LoadingView.tsx
@@ -0,0 +1,42 @@
+import React, {useMemo} from 'react';
+import {StyleSheet, View, ActivityIndicator} from 'react-native';
+import {ExtendTheme, useTheme} from '@react-navigation/native';
+import {DeviceInfo} from '~/utils/UtilDevice';
+
+interface Props {}
+const LoadingView: React.FC<Props> = () => {
+  const theme = useTheme();
+  const styles = useMemo(() => createStyles(theme), [theme]);
+  return (
+    <View style={styles.container}>
+      <View style={styles.activityIndicatorWrapper}>
+        <ActivityIndicator
+          size="small"
+          color={theme.colors.indicator}
+          animating={true}
+        />
+      </View>
+    </View>
+  );
+};
+export default React.memo(LoadingView);
+
+const createStyles = (theme: ExtendTheme) =>
+  StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      backgroundColor: theme.colors.backgroundOpacityPopup,
+      width: DeviceInfo.width,
+      height: DeviceInfo.height,
+    },
+    activityIndicatorWrapper: {
+      backgroundColor: theme.colors.background,
+      height: 80,
+      width: 80,
+      borderRadius: 10,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      alignSelf: 'center',
+    },
+  });
